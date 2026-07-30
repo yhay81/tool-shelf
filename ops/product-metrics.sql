4950a337ec61667ea405605f323b9efba0cee0b8
yusuke8h <yusuke8h@gmail.com>
2026-07-30 11:50:46 +0900

    feat: add 栞棚 to Tool Shelf

--- ops/product-metrics.sql
+++ ops/product-metrics.sql
@@ -50,5 +50,6 @@ SELECT
   COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'aikagi-ban' THEN session_id END) AS aikagi_ban,
   COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'seibi-to' THEN session_id END) AS seibi_to,
   COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'choka-to' THEN session_id END) AS choka_to,
-  COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'tsukue-no-hi' THEN session_id END) AS tsukue_no_hi
+  COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'tsukue-no-hi' THEN session_id END) AS tsukue_no_hi,
+  COUNT(DISTINCT CASE WHEN name = 'opened' AND tool = 'shiori-dana' THEN session_id END) AS shiori_dana
 FROM product_events;
