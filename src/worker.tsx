9aad1bcd73df474ec743f6f605410f0e3f040ce7
Yusuke Hayashi <yusuke8h@gmail.com>
2026-08-02 08:24:31 +0900

    Add Gakko Code to Tool Shelf (#89)

--- src/worker.tsx
+++ src/worker.tsx
@@ -88,6 +88,7 @@ const toolNames = new Set([
   "machi-npo",
   "hinan-saki",
   "ronbun-hiki",
+  "gakko-code",
 ]);
 const sessionIdPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const canonicalHostname = "tools.yhay81.com";
