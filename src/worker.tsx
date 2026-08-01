a6ce436f1c51ca10e339b2f80b0f816fd049f8b0
Yusuke Hayashi <yusuke8h@gmail.com>
2026-08-01 17:36:29 +0900

    字くらべをTool Shelfに掲載 (#68)

--- src/worker.tsx
+++ src/worker.tsx
@@ -68,6 +68,7 @@ const toolNames = new Set([
   "shutten-fuda",
   "oyako-madori",
   "deck-watashi",
+  "ji-kurabe",
 ]);
 const sessionIdPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const canonicalHostname = "tools.yhay81.com";
